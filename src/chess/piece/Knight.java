536b5f331f35f4c7dc5e9a37a5f00aa2d26a36cb
Leandro50 <leandro50.lmdo@gmail.com>
2022-07-11 11:52:03 -0400

    movendo peças

--- src/chess/piece/Knight.java
+++ src/chess/piece/Knight.java
@@ -4,14 +4,13 @@ import boardgame.Board;
 import chess.ChessPiece;
 import chess.Color;
 
-public class King extends ChessPiece {
-
-	public King(Board board, Color color) {
+public class Knight extends ChessPiece {
+	public Knight(Board board, Color color) {
 		super(board, color);
 			}
 
 	@Override
 	public String toString() {
-		return "K";
+		return "N";
 	}
 }
